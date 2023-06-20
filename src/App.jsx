df1a1024a76ebe2dd081307c38e5433529dbac2c
rio-cabalda <riojoycabalda14@gmail.com>
2023-06-20 12:01:25 +0800

    First Commit
    First Date: 20-06-2023

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Carousel from "./Components/Carousel";
+import SlickSample from "./Components/SlickSample";
+
+const App = () => {
+  return (
+    <main>
+      {/* <Carousel /> */} {/* ---use this for hard code slider*/}
+
+      <SlickSample /> {/* ---use this library for slider/carousel */}
+      {/* LIBRARY WEBSITE - https://react-slick.neostack.com/docs/get-started */}
+    </main>
+  );
+};
+export default App;
